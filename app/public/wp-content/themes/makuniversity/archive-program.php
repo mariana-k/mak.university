17b16c70570a6ebcd90f3f919ded23aafe34e596
mariana-k <mariana.kyrkosh@gmail.com>
2022-01-18 15:09:44 +0200

    Cleaner code, less duplication

--- app/public/wp-content/themes/makuniversity/archive-program.php
+++ app/public/wp-content/themes/makuniversity/archive-program.php
@@ -1,27 +1,21 @@
-<?php get_header(); ?>
-<div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
-    <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">
-            <?php the_archive_title(); ?>
-        </h1>
-        <div class="page-banner__intro">
-            <p><?php the_archive_description(); ?></p>
-        </div>
-    </div>
-</div>
+<?php get_header();
+pageBanner(array(
+    'title' => 'All Programs',
+    'subtitle' => 'There is something for everyone. Have a look around'
+));
+?>
 <div class="container container--narrow page-section">
     <ul class="link-list min-list">
-    <?php
-    while (have_posts()) {
-        the_post();
-    ?>
-        <li>
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-        </li>
-    <?php }
-    echo paginate_links();
-    ?>
+        <?php
+        while (have_posts()) {
+            the_post();
+        ?>
+            <li>
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            </li>
+        <?php }
+        echo paginate_links();
+        ?>
     </ul>
 </div>
 <?php get_footer(); ?>
